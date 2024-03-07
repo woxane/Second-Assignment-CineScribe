1ede31c05c928faa32e71d57b3f06f7d07fedd2f
woxane <mohsen.minavand1384@gmail.com>
2024-03-07 17:03:34 +0330

    Enhance Menu for Movies

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         runMenu();
@@ -7,13 +7,21 @@ public class Main {
     public static void runMenu() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Search By : \n\t1) Movie Name \n\t2) Actor Name\n\t3) EXIT");
+        System.out.println("Search For : \n\t1) Movie Name \n\t2) Actor Name\n\t3) EXIT");
 
         String userInput = scanner.nextLine();
 
         if (userInput.equals("1")) {
             System.out.println("Name of the Movie : ");
             String movieName = scanner.nextLine();
+            Movie movie = new Movie(new ArrayList<>() , "", 0);
+            String Data = movie.getMovieData(movieName);
+
+            System.out.println("Release Date : \n\t" + movie.getReleaseDateViaApi(Data));
+            System.out.println("Actors : \n\t" + movie.getActorListViaApi(Data));
+            System.out.println("Number of Votes : \n\t" + movie.getImdbVotesViaApi(Data));
+            System.out.println("Rating : \n\t" + movie.getRatingViaApi(Data));
+
 
         } else if (userInput.equals("2")) {
             System.out.println("Name of the Actor : ");
