f90d7463865ef8386936e9b79e1a3f1517b67975
woxane <mohsen.minavand1384@gmail.com>
2024-03-07 16:19:53 +0330

    Implement getMovieData function and handle possible errors

--- src/main/java/Movie.java
+++ src/main/java/Movie.java
@@ -1,11 +1,13 @@
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.io.BufferedReader;
 import java.util.ArrayList;
+import org.json.JSONObject;
 public class Movie {
-    public static final String API_KEY = "Your API_KEY";   // TODO --> add your api key about Movie here
+    public static final String API_KEY = "25ae3736";   // TODO --> add your api key about Movie here
     int ImdbVotes;
     ArrayList<String> actorsList;
     String rating;
@@ -22,19 +24,43 @@ public class Movie {
      * @return a string representation of the MovieData, or null if an error occurred
      */
 
-    public String getMovieData(String title) throws IOException {
-        URL url = new URL("https://www.omdbapi.com/?t="+title+"&apikey="+API_KEY);
-        URLConnection Url = url.openConnection();
-        Url.setRequestProperty("Authorization", "Key" + API_KEY);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(Url.getInputStream()));
-        String line;
-        StringBuilder stringBuilder = new StringBuilder();
-        while ((line = reader.readLine())!=null) {
-            stringBuilder.append(line);
+    public String getMovieData(String title) {
+        try {
+            title = title.replace(" " , "+");
+            URL url = new URL("https://www.omdbapi.com/?t="+title+"&apikey="+API_KEY);
+            URLConnection Url = url.openConnection();
+            Url.setRequestProperty("Authorization", "Key" + API_KEY);
+            if (((HttpURLConnection) Url).getResponseCode() == HttpURLConnection.HTTP_OK){
+                BufferedReader reader = new BufferedReader(new InputStreamReader(Url.getInputStream()));
+                String line;
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ((line = reader.readLine())!=null) {
+                    stringBuilder.append(line);
+                }
+
+                reader.close();
+
+                JSONObject jsonObject = new JSONObject(stringBuilder.toString());
+                String response = jsonObject.getString("Response");
+
+                if (response == "False") {
+                    return "Movie Not Found!";
+                } else {
+                    return stringBuilder.toString();
+                }
+
+            } else {
+                return "Error " + ((HttpURLConnection) Url).getResponseCode() + " : \n\t" + ((HttpURLConnection) Url).getResponseMessage();
+
+            }
+
+
+        } catch (IOException Err) {
+            Err.printStackTrace();
+            return null;
+
         }
-        reader.close();
-        //handle an error if the chosen movie is not found
-        return stringBuilder.toString();
     }
     public int getImdbVotesViaApi(String moviesInfoJson){
         //TODO --> (This function must change and return the "ImdbVotes" as an Integer)
