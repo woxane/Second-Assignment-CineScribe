4c7d4e07fdb9f56de640a97c47842c33c02ad5b9
woxane <mohsen.minavand1384@gmail.com>
2024-03-07 15:39:05 +0330

    Implement basics of runMenu function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // TODO --> complete main function
         runMenu();
     }
     public static void runMenu() {
-        // TODO
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Search By : \n\t1) Movie Name \n\t2) Actor Name\n\t3) EXIT");
+
+        String userInput = scanner.nextLine();
+
+        if (userInput.equals("1")) {
+            System.out.println("Name of the Movie : ");
+            String movieName = scanner.nextLine();
+
+        } else if (userInput.equals("2")) {
+            System.out.println("Name of the Actor : ");
+            String actorName = scanner.nextLine();
+
+        } else {
+            System.out.println("I assume you type 3.\nBa Bye");
+
+        }
+
+
     }
 }
